e4c02bccccbe1fc4202455862f02049392ccbabc
Maik Macho <nuda1998@gmail.com>
2018-09-29 11:37:16 +0200

    Update MonoMod, bump version number

---
+++ TerrariaHooks/TerrariaHooksConfig.cs
@@ -0,0 +1,17 @@
+﻿using Mono.Cecil;
+using MonoMod.RuntimeDetour;
+using MonoMod.RuntimeDetour.HookGen;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Reflection;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace TerrariaHooks {
+    class TerrariaHooksConfig {
+
+        static bool PatchOverhaul { get; set; } = true;
+
+    }
+}
--- TerrariaHooks/TerrariaHooksMod.cs
+++ TerrariaHooks/TerrariaHooksMod.cs
@@ -1,7 +1,9 @@
 ﻿using Mono.Cecil;
 using MonoMod.RuntimeDetour;
 using MonoMod.RuntimeDetour.HookGen;
+using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 using Terraria;
@@ -10,9 +12,29 @@ using Terraria.ModLoader;
 namespace TerrariaHooks {
     class TerrariaHooksMod : Mod {
 
+        static TerrariaHooksConfig Config;
+
         public TerrariaHooksMod() {
             Properties = new ModProperties() {
             };
+
+            string configPath = Path.Combine(Main.SavePath, "Mod Configs", "TerrariaHooks.json");
+
+            if (System.IO.File.Exists(configPath)) {
+                try {
+                    Config = JsonConvert.DeserializeObject<TerrariaHooksConfig>(System.IO.File.ReadAllText(configPath));
+                } catch {
+                }
+            }
+            if (Config == null) {
+                Config = new TerrariaHooksConfig();
+                string configDirPath = Path.GetDirectoryName(configPath);
+                if (!Directory.Exists(configDirPath));
+                    Directory.CreateDirectory(configDirPath);
+                if (System.IO.File.Exists(configPath))
+                    System.IO.File.Delete(configPath);
+                System.IO.File.WriteAllText(configPath, JsonConvert.SerializeObject(Config));
+            }
 
             // Required because Terraria ships with some dependencies as embedded resources.
             HookEndpointManager.OnGenerateCecilModule += name => {
@@ -31,12 +53,20 @@ namespace TerrariaHooks {
                 typeof(Mod).GetMethod("UnloadContent", BindingFlags.NonPublic | BindingFlags.Instance),
                 typeof(TerrariaHooksMod).GetMethod("OnUnloadContent", BindingFlags.NonPublic | BindingFlags.Static)
             );
+
+            // Some mods ship with their own detour ("method swapping") implementations.
+            // Those conflict with each other, and with RuntimeDetour in TerrariaHooks.
+            // TODO
         }
 
         static Hook HookOnUnloadContent;
         static void OnUnloadContent(Action<Mod> orig, Mod mod) {
-            // Unload any HookGen hooks after unloading the mod.
+            // Undo any fixes performed by TerrariaHooks.
+            // TODO
+
             orig(mod);
+
+            // Unload any HookGen hooks after unloading the mod.
             HookEndpointManager.RemoveAllOwnedBy(mod.Code);
         }
 
